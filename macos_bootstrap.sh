3c0981a6b42487f40a220ec5dcd112d77b95a3ca
Nikolai Shelekhov <nickshv13@gmail.com>
2021-02-18 18:06:13 +0800

    Update macos_bootstrap.sh
    .

--- macos_bootstrap.sh
+++ macos_bootstrap.sh
@@ -20,9 +20,6 @@
 # - https://news.ycombinator.com/item?id=8402079
 # - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
 
-sudo visudo
-Defaults        timestamp_timeout=60
-
 echo "Starting bootstrapping"
 
 # Check for Homebrew, install if we don't have it
