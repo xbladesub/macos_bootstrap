d08b5b96c0f21518e8a61170b619f784a106c7d5
Nikolai Shelekhov <nickshv13@gmail.com>
2021-02-18 23:23:54 +0800

    Update and rename macos_bootstrap.sh to install.sh
    ..

--- install.sh
+++ install.sh
@@ -43,8 +43,11 @@ PACKAGES=(
 )
 brew install ${PACKAGES[@]}
 
-#Install ohmyzsh
+#Install terminal adds
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 echo "Cleaning up..."
 brew cleanup
