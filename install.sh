e4df19f8a37722042016f062419b7e10e79477cb
Nikolai Shelekhov <nickshv13@gmail.com>
2021-03-03 12:52:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ echo "Starting bootstrapping"
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
     echo "Installing homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update homebrew recipes
